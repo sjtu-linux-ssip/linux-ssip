535c5e78ddf002aa0ed2499991e6ac88ae15b744
Zi-Han Liu <52587532+zhliuworks@users.noreply.github.com>
2021-10-16 17:00:01 +0800

    kmod: mod_sys_openat_write_hook (#3)
    * fix: uid/gid aggregate val bug
    
    * kmod: mod_sys_openat_write_hook

--- modules/sys_kill_hook/mod_sys_kill_hook.c
+++ modules/sys_kill_hook/mod_sys_kill_hook.c
@@ -7,7 +7,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("https://github.com/SanqingQi/Integrity_Protection_IS415");
-MODULE_DESCRIPTION("a loadable kernel module to hook `kill syscall`, for linux kernel v5.4.0");
+MODULE_DESCRIPTION("a loadable kernel module to hook `sys_kill`, for linux kernel v5.4.0");
 
 typedef void (*syscall_ptr_t)(void);
 syscall_ptr_t *syscall_table = NULL;
@@ -21,25 +21,39 @@ pte_t *pte;
 // protected pid
 static int protected_pid;
 module_param(protected_pid, int, 0644);
-// threat uid/gid (-1 if all)
-static int threat_uid;
-static int threat_gid;
+// threat uid/gid (-1: all except root)
+static int threat_uid = -1;
+static int threat_gid = -1;
 module_param(threat_uid, int, 0644);
 module_param(threat_gid, int, 0644);
 
-// overloaded syscall handler
+// send SIGKILL to kill threat process
+int kill_threat_process(struct task_struct *task) {
+	struct kernel_siginfo info;
+	memset(&info, 0, sizeof(struct kernel_siginfo));
+	info.si_signo = SIGKILL;
+	return send_sig_info(SIGKILL, &info, task);
+}
+
+
+// overloaded sys_kill handler
 asmlinkage long sys_kill_hook(const struct pt_regs *regs) {
 
-	pid_t pid = regs->di;
-	int sig = regs->si;
-
-	if (pid == protected_pid && sig == SIGKILL) {
-		if (threat_uid == -1 || threat_uid == current->cred->uid) {
-			if (threat_gid == -1 || threat_gid == current->cred->gid) {
-				printk("potential threat detected!");
-				printk("attacker pid: %d, uid: %d, gid: %d", current->pid, current->cred->uid, current->cred->gid);
-				printk("protect victim process %d from killed", protected_pid);
-				return 0;
+	if (regs->di == protected_pid && regs->si == SIGKILL) {
+		if ((current->cred->uid).val && (current->cred->gid).val) {
+			if (threat_uid == -1 || threat_uid == (current->cred->uid).val) {
+				if (threat_gid == -1 || threat_gid == (current->cred->gid).val) {
+					printk("potential threat detected!");
+					printk("attacker pid: %d, uid: %d, gid: %d",
+						current->pid, (current->cred->uid).val, (current->cred->gid).val);
+					printk("protect victim process %d from killed", protected_pid);
+					if (kill_threat_process(current) < 0) {
+						printk("error sending signal");
+					} else {
+						printk("killed responsible process");
+						return 0;
+					}
+				}
 			}
 		}
 	}
---
+++ modules/sys_openat_write_hook/README.md
@@ -0,0 +1,16 @@
+## Module `mod_sys_openat_write_hook`
+
+* Linux Kernel version 5.4.0
+* Protect certain file from written by certain user/group, and kill the responsible process
+
+### Usage
+
+```shell
+>> make
+>> insmod mod_sys_openat_write_hook.ko \
+protected_filename=<xxx> \
+threat_uid=<xxx> \  # protect from all users except root, NOT specify this parameter
+threat_gid=<xxx>    # protect from all groups except root, NOT specify this parameter
+>> rmmod mod_sys_openat_write_hook
+```
+
